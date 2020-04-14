a8f5bb7eb22f551125997e086e45c572bca7e069
tgmichel <telmo@purestake.com>
2020-04-14 11:11:27 +0200

    Added build helper script

---
+++ build.sh
@@ -0,0 +1,21 @@
+rm -r dist
+mkdir dist
+
+# extension-library
+cd packages/extension\-library && npm install && npm run bundle
+cp dist/AlgoSigner.min.js ../../dist
+rm -r dist
+
+# extension
+cd ../../packages/extension && npm install && tsc && cd build
+cp content/content.js ../../../dist # content
+cp background/background.js ../../../dist # background
+cp ../manifest.json ../../../dist # manifest
+cp ../public/images/icon.png ../../../dist # icon
+cd .. && rm -r build
+
+# extension-ui
+cd ../../packages/extension\-ui && npm install && tsc && cd build
+cp popup.js ../../../dist # popup
+cp ../popup.html ../../../dist # popup html
+cd .. && rm -r build
