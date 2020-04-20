55a5537253b9a2074dbc30ae59398d2b9820d15f
tgmichel <telmo@purestake.com>
2020-04-20 10:03:37 +0200

    injected AlgoSigner to singleton

--- packages/extension-dapp/src/algosigner.ts
+++ packages/extension-dapp/src/algosigner.ts
@@ -4,11 +4,19 @@ import {Router} from './fn/router';
 import {MessageBuilder} from './messaging/builder'; 
 import {JsonRpcMethod,JsonRpcResponse} from '@algosigner/common/messaging/types';
 
-// should AlgoSigner be a singleton?
-export class AlgoSigner {
-    clerk = new Clerk();
-    send = this.clerk.send;
-    router = new Router();
+class Wrapper {
+    private static instance: Wrapper;
+    private clerk: Clerk = new Clerk();
+    private router: Router = new Router();
+    public send: Function = this.clerk.send;
+
+    public static getInstance(): Wrapper {
+        if (!Wrapper.instance) {
+            Wrapper.instance = new Wrapper();
+        }
+        return Wrapper.instance;
+    }
+
     connect(): Promise<JsonRpcResponse> {
         return MessageBuilder.promise(
             JsonRpcMethod.Authorization, 
@@ -16,3 +24,5 @@ export class AlgoSigner {
         );
     }
 }
+
+export const AlgoSigner = Wrapper.getInstance();
