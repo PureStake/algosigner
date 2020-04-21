976bc2aa4077771356b4b9ab78f2635a62865ba9
tgmichel <telmo@purestake.com>
2020-04-21 15:55:24 +0200

    add MessageApi platform switch

--- packages/extension/background/messaging/api.ts
+++ packages/extension/background/messaging/api.ts
@@ -12,13 +12,16 @@ export class MessageApi {
         }
     }
     public static send(d: any,active_tab: boolean = true) {
-        if(active_tab) {
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-                var tab_id = tabs[0].id || 0;
-                chrome.tabs.sendMessage(tab_id,d);
-            });
-        } else {
-            // TODO all tabs?
-        }
+        switch(PLATFORM) {
+            case 'chrome':
+                if(active_tab) {
+                    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+                        var tab_id = tabs[0].id || 0;
+                        chrome.tabs.sendMessage(tab_id,d);
+                    });
+                } else {
+                    // TODO all tabs?
+                }
+                break;
     }
 }
