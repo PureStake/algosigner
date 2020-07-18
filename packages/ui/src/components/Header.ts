0637b0b494342b6e20496d1d76057758bc86a844
Francisco Gamundi <francisco@purestake.com>
2020-07-18 04:00:31 +0200

    Added logotype to header

--- packages/ui/src/components/Header.ts
+++ packages/ui/src/components/Header.ts
@@ -3,12 +3,12 @@ import { html } from 'htm/preact';
 
 import LedgerSelect from './LedgerSelect'
 
+import logotype from 'assets/logotype.png'
+
 const Header: FunctionalComponent = () => {
   return html`
-    <div class="container is-fluid" style="flex: 0; display: flex; align-items: center; justify-content: space-between; border-bottom: 1px solid #EFF4F7">
-      <span>
-        AlgoSigner
-      </span>
+    <div class="px-4" style="flex: 0; display: flex; align-items: center; justify-content: space-between; border-bottom: 1px solid #EFF4F7">
+      <img src=${logotype} width="130" />
       <div>
         <${LedgerSelect} />
       </div>
