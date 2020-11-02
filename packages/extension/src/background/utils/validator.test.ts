748386c081f05b12958bf6b535991f4143b4a467
Brent <Brent@purestake.com>
2020-11-02 09:47:38 -0500

    Spelling fix

--- packages/extension/src/background/utils/validator.test.ts
+++ packages/extension/src/background/utils/validator.test.ts
@@ -1,68 +1,68 @@
 import { Validate, ValidationStatus } from  "./validator";
 
-test('Valitdate correct to address', () => {
+test('Validate correct to address', () => {
     let result = Validate("to", "NM2MBC673SL7TQIKUXD4JOBR3XQITDCHIMIEODQBUGFMAN54QV2VUYWZNQ");
     expect(result.status).toBe(ValidationStatus.Valid);
 });
 
-test('Valitdate invalid to address', () => {
+test('Validate invalid to address', () => {
     let result = Validate("to", "12345");
     expect(result.status).toBe(ValidationStatus.Invalid);
 });
 
-test('Valitdate correct amount', () => {
+test('Validate correct amount', () => {
     let result = Validate("amount", 1);
     expect(result.status).toBe(ValidationStatus.Valid);
 });
 
-test('Valitdate invalid amount', () => {
+test('Validate invalid amount', () => {
     let result = Validate("amount", 9999999999999999999999999);
     expect(result.status).toBe(ValidationStatus.Invalid);
 });
 
-test('Valitdate normal fee', () => {
+test('Validate normal fee', () => {
     let result = Validate("fee", 1000);
     expect(result.status).toBe(ValidationStatus.Valid);
 });
 
-test('Valitdate elevated fee', () => {
+test('Validate elevated fee', () => {
     let result = Validate("fee", 100000);
     expect(result.status).toBe(ValidationStatus.Warning);
 });
 
-test('Valitdate very high fee', () => {
+test('Validate very high fee', () => {
     let result = Validate("fee", 10000000);
     expect(result.status).toBe(ValidationStatus.Dangerous);
 });
 
-test('Valitdate closeRemainderTo empty', () => {
+test('Validate closeRemainderTo empty', () => {
     let result = Validate("closeRemainderTo", "");
     expect(result.status).toBe(ValidationStatus.Valid);
 });
 
-test('Valitdate closeRemainderTo', () => {
+test('Validate closeRemainderTo', () => {
     let result = Validate("closeRemainderTo", "NM2MBC673SL7TQIKUXD4JOBR3XQITDCHIMIEODQBUGFMAN54QV2VUYWZNQ");
     expect(result.status).toBe(ValidationStatus.Dangerous);
 });
 
-test('Valitdate correct assetIndex', () => {
+test('Validate correct assetIndex', () => {
     let result = Validate("assetIndex", 1);
     expect(result.status).toBe(ValidationStatus.Valid);
 });
 
-test('Valitdate invalid assetIndex', () => {
+test('Validate invalid assetIndex', () => {
     let result = Validate("assetIndex", 9999999999999999999999999);
     expect(result.status).toBe(ValidationStatus.Invalid);
 });
 
-test('Valitdate correct rounds', () => {
+test('Validate correct rounds', () => {
     let resultFirst = Validate("firstRound", 1);
     let resultLast = Validate("lastRound", 1);
     expect(resultFirst.status).toBe(ValidationStatus.Valid);
     expect(resultLast.status).toBe(ValidationStatus.Valid);
 });
 
-test('Valitdate invalid rounds', () => {
+test('Validate invalid rounds', () => {
     let resultFirst = Validate("firstRound", 9999999999999999999999999);
     let resultLast = Validate("lastRound", 9999999999999999999999999);
     expect(resultFirst.status).toBe(ValidationStatus.Invalid);
