7ac74a3c327fdeb14571e86c4145e7d60f92141f
Jan Marcano <jan@purestake.com>
2021-02-12 23:20:43 -0300

    Add 'name' and 'tag' SDK fields to pass validation

--- packages/extension/src/background/transaction/axferClawbackTransaction.ts
+++ packages/extension/src/background/transaction/axferClawbackTransaction.ts
@@ -1,35 +1,37 @@
-import { IAssetClawbackTx } from "@algosigner/common/interfaces/axfer_clawback";
-import { BaseValidatedTxnWrap } from "./baseValidatedTxnWrap";
+import { IAssetClawbackTx } from '@algosigner/common/interfaces/axfer_clawback';
+import { BaseValidatedTxnWrap } from './baseValidatedTxnWrap';
 
 ///
 // Base implementation of the transactions type interface, for use in the export wrapper class below.
 ///
 class AssetClawbackTx implements IAssetClawbackTx {
-    type: string = undefined;
-    assetIndex: number = undefined;
-    amount: number = undefined;
-    closeRemainderTo?: string = null;
-    from: string = undefined;
-    to: string = undefined;
-    fee: number = undefined;
-    firstRound: number = undefined;
-    lastRound: number = undefined;
-    note?: string = null;
-    genesisID: string = undefined;
-    genesisHash: any = undefined;
-    group?: any = null;
-    lease?: any = null;
-    reKeyTo?: any = null;
-    assetRevocationTarget: string = undefined;
-    flatFee?: any = null;
+  type: string = undefined;
+  assetIndex: number = undefined;
+  amount: number = undefined;
+  closeRemainderTo?: string = null;
+  from: string = undefined;
+  to: string = undefined;
+  fee: number = undefined;
+  firstRound: number = undefined;
+  lastRound: number = undefined;
+  note?: string = null;
+  genesisID: string = undefined;
+  genesisHash: any = undefined;
+  group?: any = null;
+  lease?: any = null;
+  reKeyTo?: any = null;
+  assetRevocationTarget: string = undefined;
+  flatFee?: any = null;
+  name?: string = null;
+  tag?: any = null;
 }
 
 ///
 // Mapping, validation and error checking for axfer clawback transactions prior to sign.
 ///
 export class AssetClawbackTransaction extends BaseValidatedTxnWrap {
-    txDerivedTypeText: string = 'Asset Clawback';
-    constructor(params: IAssetClawbackTx){   
-        super(params, AssetClawbackTx);
-    }
+  txDerivedTypeText: string = 'Asset Clawback';
+  constructor(params: IAssetClawbackTx) {
+    super(params, AssetClawbackTx);
+  }
 }
--- packages/extension/src/background/transaction/keyregTransaction.ts
+++ packages/extension/src/background/transaction/keyregTransaction.ts
@@ -1,35 +1,37 @@
-import { IKeyRegistrationTx } from "@algosigner/common/interfaces/keyreg";
-import { BaseValidatedTxnWrap } from "./baseValidatedTxnWrap";
+import { IKeyRegistrationTx } from '@algosigner/common/interfaces/keyreg';
+import { BaseValidatedTxnWrap } from './baseValidatedTxnWrap';
 
 ///
 // Base implementation of the transactions type interface, for use in the export wrapper class below.
 ///
 class KeyRegistrationTx implements IKeyRegistrationTx {
-    type: string = undefined;
-    voteKey: string = undefined;
-    selectionKey: string = undefined;
-    voteFirst: number = undefined;
-    voteLast: number = undefined;
-    voteKeyDilution?: number = null;
-    from: string = undefined;
-    fee: number = undefined;
-    firstRound: number = undefined;
-    lastRound: number = undefined;
-    note?: string = null;
-    genesisID: string = undefined;
-    genesisHash: any = undefined;
-    group?: any = null;
-    lease?: any = null;
-    reKeyTo?: any = null;
-    flatFee?: any = null;
+  type: string = undefined;
+  voteKey: string = undefined;
+  selectionKey: string = undefined;
+  voteFirst: number = undefined;
+  voteLast: number = undefined;
+  voteKeyDilution?: number = null;
+  from: string = undefined;
+  fee: number = undefined;
+  firstRound: number = undefined;
+  lastRound: number = undefined;
+  note?: string = null;
+  genesisID: string = undefined;
+  genesisHash: any = undefined;
+  group?: any = null;
+  lease?: any = null;
+  reKeyTo?: any = null;
+  flatFee?: any = null;
+  name?: string = null;
+  tag?: any = null;
 }
 
 ///
 // Mapping, validation and error checking for keyreg transactions prior to sign.
 ///
-export class KeyregTransaction  extends BaseValidatedTxnWrap {
-    txDerivedTypeText: string = 'Key Registration';
-    constructor(params: IKeyRegistrationTx){   
-        super(params, KeyRegistrationTx);
-    }
+export class KeyregTransaction extends BaseValidatedTxnWrap {
+  txDerivedTypeText: string = 'Key Registration';
+  constructor(params: IKeyRegistrationTx) {
+    super(params, KeyRegistrationTx);
+  }
 }
