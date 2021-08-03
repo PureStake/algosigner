c6293f315288706010974f9bcdd2d43702557f02
Jan Marcano <jan@purestake.com>
2021-08-03 12:03:05 -0300

    Support BigInt serialization

--- packages/extension/src/background/transaction/baseValidatedTxnWrap.ts
+++ packages/extension/src/background/transaction/baseValidatedTxnWrap.ts
@@ -30,12 +30,10 @@ export class BaseValidatedTxnWrap {
     const missingFields = [];
     const extraFields = [];
 
-    // if(v1Validations) {
-    //   this.validityObject['v1'] = new ValidationResponse({
-    //     status: ValidationStatus.Warning,
-    //     info: 'Version 1 transactions have been deprecated.',
-    //   });
-    // }
+    // Add support for BigInt serialization
+    (BigInt as any).prototype.toJSON = function () {
+      return this.toString();
+    };
 
     // Cycle base transaction fields for this type of transaction to verify require fields are present.
     // Nullable type fields are being initialized to null instead of undefined.
