0121df76e0cbd302e4012c8b3a4fa10b279d0db4
Jan Marcano <jan@purestake.com>
2022-08-23 12:48:29 -0300

    Update e2e tests for Account Creation

--- packages/test-project/tests/ui-accounts-e2e.test.js
+++ packages/test-project/tests/ui-accounts-e2e.test.js
@@ -5,7 +5,7 @@
  */
 
 const { wallet } = require('./common/constants');
-const { openExtension } = require('./common/helpers');
+const { openExtension, getOpenedTab } = require('./common/helpers');
 const { CreateWallet, VerifyAccount, DeleteAccount } = require('./common/tests');
 
 describe('Wallet Setup', () => {
@@ -22,67 +22,78 @@ describe('Create Account', () => {
     name: 'Created-Account',
   };
   let mnemonicArray = [];
+  let openedTab;
 
-  test('Create An Account, Step 1 - Enter Account Name', async () => {
+  test('Create An Account, Step 0 - Open Account creation tab', async () => {
     await extensionPage.waitForSelector('#addAccount');
     await extensionPage.click('#addAccount');
     await extensionPage.waitForSelector('#createAccount');
     await extensionPage.click('#createAccount');
-    await extensionPage.type('#setAccountName', createdAccount.name);
-    await extensionPage.click('#nextStep');
+    openedTab = await getOpenedTab();
+  });
+
+  test('Create An Account, Step 1 - Enter Account Name', async () => {
+    await openedTab.type('#setAccountName', createdAccount.name);
+    await openedTab.click('#nextStep');
   });
 
   test('Create An Account, Step 2 - Get Mnemonic', async () => {
-    await extensionPage.click('#accountAddress');
-    createdAccount.address = await extensionPage.$eval('#accountAddress', (e) => e.innerText); // setup for another test
+    await openedTab.click('#accountAddress');
+    createdAccount.address = await openedTab.$eval('#accountAddress', (e) => e.innerText); // setup for another test
 
     for (let i = 1; i <= 25; i++) {
-      mnemonicArray[i - 1] = await extensionPage.$eval(
+      mnemonicArray[i - 1] = await openedTab.$eval(
         `[data-key-index="${i}"]`,
         (e) => e.dataset['word']
       );
     }
-    await extensionPage.waitForSelector('#recordCheckbox');
-    await extensionPage.click('#recordCheckbox');
-    await extensionPage.click('#nextStep');
+    await openedTab.waitForSelector('#recordCheckbox');
+    await openedTab.click('#recordCheckbox');
+    await openedTab.click('#nextStep');
   });
 
   test('Create An Account, Step 3 - Use Mnemonic', async () => {
     // Wait for mnemonic word buttons to be available
-    await extensionPage.waitForSelector('[data-index]:not([disabled])');
+    await openedTab.waitForSelector('[data-index]:not([disabled])');
 
     // We test the remove word functionality by first filling with random words
     const randomWords = [];
     for (let i = 0; i < 5; i++) {
       randomWords.push(mnemonicArray[i * 5 + Math.round(Math.random() * 4)]);
-      await extensionPage.click(`#${randomWords[i]}:not([disabled]):not(.is-link)`);
+      await openedTab.click(`#${randomWords[i]}:not([disabled]):not(.is-link)`);
     }
-    await expect(extensionPage.$eval('#enterMnemonic', (e) => e.value)).resolves.toBe(
+    await expect(openedTab.$eval('#enterMnemonic', (e) => e.value)).resolves.toBe(
       randomWords.join(' ')
     );
-    await extensionPage.waitForTimeout(200);
+    await openedTab.waitForTimeout(200);
     for (let i = 0; i < 5; i++) {
-      await extensionPage.click(`button.is-small.is-link`);
+      await openedTab.click(`button.is-small.is-link`);
     }
-    await expect(extensionPage.$eval('#enterMnemonic', (e) => e.value)).resolves.toBe('');
-    await extensionPage.waitForTimeout(200);
+    await expect(openedTab.$eval('#enterMnemonic', (e) => e.value)).resolves.toBe('');
+    await openedTab.waitForTimeout(200);
 
     // We build the actual mnemonic
     for (let i = 0; i < 25; i++) {
-      await extensionPage.click(`#${mnemonicArray[i]}:not([disabled]):not(.is-link)`);
+      await openedTab.click(`#${mnemonicArray[i]}:not([disabled]):not(.is-link)`);
     }
-    await expect(extensionPage.$eval('#enterMnemonic', (e) => e.value)).resolves.toBe(
+    await expect(openedTab.$eval('#enterMnemonic', (e) => e.value)).resolves.toBe(
       mnemonicArray.join(' ')
     );
 
-    await extensionPage.click('#nextStep');
+    await openedTab.click('#nextStep');
   });
 
   test('Create an Account, Step 4 - Write Account into Storage', async () => {
-    await extensionPage.waitForSelector('#enterPassword');
-    await extensionPage.type('#enterPassword', wallet.password);
-    await extensionPage.waitForTimeout(200);
-    await extensionPage.click('#authButton');
+    await openedTab.waitForSelector('#enterPassword');
+    await openedTab.type('#enterPassword', wallet.password);
+    await openedTab.waitForTimeout(200);
+    await openedTab.click('#authButton');
+    await openedTab.waitForSelector('[data-account-name]');
+    await expect(
+      openedTab.$eval('[data-account-name]', (e) => e.dataset['accountName'])
+    ).resolves.toBe(createdAccount.name);
+    await openedTab.close();
+    await extensionPage.reload();
   });
 
   VerifyAccount(createdAccount);
